50b0b31e54f316890d25cdf57bf99fe086127ada
raozijian <zijian.rao@chukong-inc.com>
2013-11-12 13:46:42 +0800

    create main menu scene

--- cocos2d-x-2.2/projects/ParkourSample/Classes/AppDelegate.cpp
+++ cocos2d-x-2.2/projects/ParkourSample/Classes/AppDelegate.cpp
@@ -1,4 +1,5 @@
 #include "AppDelegate.h"
+#include "MainMenuScene.h"
 
 USING_NS_CC;
 
@@ -24,10 +25,11 @@ bool AppDelegate::applicationDidFinishLaunching() {
     pDirector->setAnimationInterval(1.0 / 60);
 
     // create a scene. it's an autorelease object
-    //CCScene *pScene = HelloWorld::scene();
+   CCScene *pScene = new MainMenuScene();
+   pScene->init();
 
     // run
-    //pDirector->runWithScene(pScene);
+    pDirector->runWithScene(pScene);
 
     return true;
 }
